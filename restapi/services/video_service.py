af81b2bb3cf4902b2d35cd23957c65e3a581f2ce
Nilesh <nick.aggarwal@gmail.com>
2019-07-26 12:59:33 +0530

    Changes for Video Service

--- restapi/services/video_service.py
+++ restapi/services/video_service.py
@@ -4,7 +4,7 @@ import requests
 
 class VideoService():
 
-    BASE_URL = "http://webapp:8080/static/{}"
+    BASE_URL = "http://restapp:8080/static/{}"
     ProcessedFile = "video-processed-{}.mp4"
 
     @staticmethod
@@ -13,18 +13,18 @@ class VideoService():
         name = video_url.rsplit('/', 1)[1]
         r = requests.get(video_url, allow_redirects=True)
         open('static/'+name, 'wb').write(r.content)
-        clip = VideoFileClip('static/'+name)
+        clip = VideoFileClip('static/' + name)
         if clip.duration < interval_time:
             result.append({"video_url": VideoService.BASE_URL.format(name)})
         else:
             no_of_file = int(int(clip.duration) / interval_time) + 1
             for i in range(0, no_of_file):
                 new_name = VideoService.ProcessedFile.format(i)
+                clip = VideoFileClip('static/' + name)
                 start = i*interval_time
                 end = min((i+1)*interval_time, clip.duration)
                 clip.subclip(start, end).write_videofile("static/"+new_name)
-
-                result.append({"video_url" : VideoService.BASE_URL.format(new_name)})
+                result.append({"video_url": VideoService.BASE_URL.format(new_name)})
 
         return {"interval_videos": result}
 
@@ -34,9 +34,9 @@ class VideoService():
         name = video_url.rsplit('/', 1)[1]
         r = requests.get(video_url, allow_redirects=True)
         open('static/'+name, 'wb').write(r.content)
-        clip = VideoFileClip('static/'+name)
         i = 0
         for part in ranges:
+            clip = VideoFileClip('static/' + name)
             new_name = VideoService.ProcessedFile.format(i)
             clip.subclip(part.get("start"), part.get("end")).write_videofile("static/"+new_name)
             result.append({"video_url": VideoService.BASE_URL.format(new_name)})
@@ -53,6 +53,7 @@ class VideoService():
         clip = VideoFileClip('static/' + name)
         interval_time = int( clip.duration) / no_of_file
         for i in range(0, no_of_file):
+            clip = VideoFileClip('static/' + name)
             new_name = VideoService.ProcessedFile.format(i)
             start = i * interval_time
             end = min((i + 1) * interval_time, clip.duration)
